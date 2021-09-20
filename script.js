7a05acc2071f668de113f78c1a08f31b0117e551
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-09-20 14:46:45 +0100

    remove added books

--- script.js
+++ script.js
@@ -9,12 +9,30 @@ const AddButton = document.querySelector('#add-button')
 
 AddButton.addEventListener('click', ()=>{
 
+  const Books= document.createElement('ul')
+  Booklist.appendChild(Books)
+
  
   const Author = document.createElement ('li')
   const Title = document.createElement ('li')
+  const Remove = document.createElement('input')
+  const line = document.createElement('hr')
+  const Break = document.createElement('br')
+
+  Books.appendChild(Author)
+  Books.appendChild(Title)
+  Books.appendChild(Remove)
+  Books.appendChild(line)
+  Books.appendChild(Break)
+
+  Remove.setAttribute('type','button')
+  Remove.setAttribute('value','Remove')
+  Remove.addEventListener('click', ()=>{
+    Booklist.removeChild(Books)
+  })
   Author.innerHTML=AuthorValue.value
   Title.innerHTML=TitleValue.value
-  Booklist.appendChild(Author)
-  Booklist.appendChild(Title)
+ 
+
 
 });
