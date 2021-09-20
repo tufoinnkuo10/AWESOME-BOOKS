36bd12ab6759863cf9558b6ffd051c5c1e00e3e8
Marc Raphael <marcraphael979@gmail.com>
2021-09-20 15:50:04 +0100

    Add local storage, and empty fields popup message

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
+const Booklist = document.querySelector('.book-list');
+const AuthorValue = document.querySelector('.author');
+const TitleValue = document.querySelector('.title');
+const AddButton = document.querySelector('#add-button');
+const inputs = document.querySelectorAll('.author, .title');
 
-const BookCollection = [];
-const Booklist= document.querySelector('.book-list');
+const saveData = (user, data) => localStorage.setItem(user, JSON.stringify(data));
+const getData = (user) => JSON.parse(localStorage.getItem(user));
 
-const AuthorValue = document.querySelector('.author')
-const TitleValue = document.querySelector('.title')
+inputs.forEach((e) => {
+  e.addEventListener('input', () => {
+    const user = {
+      author: AuthorValue.value || 'Your book doesn\'t have an Author',
+      book: TitleValue.value || 'Your book doesn\'t have a Title',
+    };
+    saveData('libraryData', user);
+  });
+});
 
-const AddButton = document.querySelector('#add-button')
+AddButton.addEventListener('click', () => {
+  const Books = document.createElement('ul');
+  Booklist.appendChild(Books);
 
-AddButton.addEventListener('click', ()=>{
+  const Author = document.createElement('li');
+  const Title = document.createElement('li');
+  const Remove = document.createElement('input');
+  const line = document.createElement('hr');
+  const Break = document.createElement('br');
 
-  const Books= document.createElement('ul')
-  Booklist.appendChild(Books)
+  const libraryData = getData('libraryData');
+  if (libraryData !== null) {
+    Author.innerHTML = libraryData.author;
+    Title.innerHTML = libraryData.book;
+  }
 
- 
-  const Author = document.createElement ('li')
-  const Title = document.createElement ('li')
-  const Remove = document.createElement('input')
-  const line = document.createElement('hr')
-  const Break = document.createElement('br')
+  Books.appendChild(Author);
+  Books.appendChild(Title);
+  Books.appendChild(Remove);
+  Books.appendChild(line);
+  Books.appendChild(Break);
 
-  Books.appendChild(Author)
-  Books.appendChild(Title)
-  Books.appendChild(Remove)
-  Books.appendChild(line)
-  Books.appendChild(Break)
-
-  Remove.setAttribute('type','button')
-  Remove.setAttribute('value','Remove')
-  Remove.addEventListener('click', ()=>{
-    Booklist.removeChild(Books)
-  })
-  Author.innerHTML=AuthorValue.value
-  Title.innerHTML=TitleValue.value
- 
-
-
+  Remove.setAttribute('type', 'button');
+  Remove.setAttribute('value', 'Remove');
+  Remove.addEventListener('click', () => {
+    Booklist.removeChild(Books);
+  });
+  AuthorValue.value = null;
+  TitleValue.value = null;
 });
